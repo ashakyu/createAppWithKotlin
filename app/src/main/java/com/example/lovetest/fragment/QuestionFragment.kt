14200d8cb5cb8fb36c9b6a826ba5eb36eb897c33
EunJi Kim <ejdd9630@gmail.com>
2022-05-17 23:15:03 +0900

    love Test application done

--- app/src/main/java/com/example/lovetest/fragment/QuestionFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/QuestionFragment.kt
@@ -1,23 +1,26 @@
-package com.example.lovetest.fragment
+package com.junga.lovetest.fragment
+
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.lovetest.R
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.junga.lovetest.R
+import kotlinx.android.synthetic.main.fragment_question.*
 
 /**
  * A simple [Fragment] subclass.
- * Use the [QuestionFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
-class QuestionFragment : Fragment() {
+class QuestionFragment : Fragment(), View.OnClickListener{
+
+
+    lateinit var navController : NavController
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,4 +28,26 @@ class QuestionFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_question, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        btn_next.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_next -> {
+                navController.navigate(R.id.action_questionFragment_to_selectionFragment)
+            }
+
+        }
+    }
+
+
+
+
 }
